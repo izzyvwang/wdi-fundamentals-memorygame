1638ae02fa41700410256f8cec5273e782a7717e
Izzy <isabelle.wang@disney.com>
2018-01-07 01:48:06 -0800

    Unit 11 - added events and game reset

--- js/main.js
+++ js/main.js
@@ -18,20 +18,38 @@ cardImage: "images/king-of-hearts.png"
  ];
 var cardsInPlay = [];
 var checkForMatch = function()
-{
-	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1])
+{if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1])
 {alert("You found a match!");}
 else {
 	alert("Sorry, try again.")};
 };
-var flipCard = function(cardId)
-{
+
+var flipCard = function()
+{	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+	this.setAttribute("src", cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();}
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function()
+{
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+};
+};
+
+var resetBoard = function() {
+	location.reload()}
+	
+createBoard();
+
+document.querySelector('button').addEventListener('click', resetBoard);
+
